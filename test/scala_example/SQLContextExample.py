df762f982a9d7887a8b5906ebe915d4b27315621
CHANDER PAL <44088072+chander-pal@users.noreply.github.com>
2024-04-23 02:13:05 +0530

    Automated conversion from Scala to PySpark

---
+++ test/scala_example/SQLContextExample.py
@@ -0,0 +1,49 @@
+# Scala code:
+# package com.sparkbyexamples.spark
+# 
+# import org.apache.spark.sql.{SQLContext, SparkSession}
+# 
+# object SQLContextExample extends App {
+# 
+#   val spark = SparkSession.builder()
+#     .master("local[1]")
+#     .appName("SparkByExamples.com")
+#     .getOrCreate();
+# 
+#   spark.sparkContext.setLogLevel("ERROR")
+# 
+# 
+#   val sqlContext:SQLContext = spark.sqlContext
+# 
+#   //read csv with options
+#   val df = sqlContext.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
+#     .csv("src/main/resources/zipcodes.csv")
+#   df.show()
+#   df.printSchema()
+# 
+#   df.createOrReplaceTempView("TAB")
+#   sqlContext.sql("select * from TAB")
+#     .show(false)
+# 
+# }
+# 
+
+# PySpark equivalent provided by GPT:
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("SparkByExamples.com") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
+
+# read csv with options
+df = spark.read.options(inferSchema="true", delimiter=",", header="true") \
+    .csv("src/main/resources/zipcodes.csv")
+
+df.show()
+df.printSchema()
+
+df.createOrReplaceTempView("TAB")
+spark.sql("select * from TAB") \
+    .show(False)
