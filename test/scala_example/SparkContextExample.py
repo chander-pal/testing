0ef565873cddc1e569633dcd73af2e9e5ba16c9a
CHANDER PAL <44088072+chander-pal@users.noreply.github.com>
2024-04-23 12:24:25 +0530

    Automated conversion from Scala to PySpark

--- test/scala_example/SparkContextExample.py
+++ test/scala_example/SparkContextExample.py
@@ -4,6 +4,18 @@
 # # # # Scala code:
 # # # # # Scala code:
 # # # # # # Scala code:
+# # # # # # # Scala code:
+# # # # # 
+# # # # # # PySpark equivalent provided by GPT:
+# # # # # from pyspark.sql import SparkSession
+# # # # # 
+# # # # # spark = SparkSession.builder.getOrCreate()
+# # # # # sc = spark.sparkContext
+# # # # # 
+# # # # # data = [1, 2, 3, 4, 5]
+# # # # # rdd = sc.parallelize(data)
+# # # # # filteredRDD = rdd.filter(lambda x: x % 2 == 0)
+# # # # # filteredRDD.collect()
 # # # # 
 # # # # # PySpark equivalent provided by GPT:
 # # # # from pyspark.sql import SparkSession
